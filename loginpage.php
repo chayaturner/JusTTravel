241bc00cd674a2a5c872efdf439fb357cd0e20e2
chayaturner <dietaholic@gmail.com>
2016-10-06 15:36:33 -0400

    Added login option

--- loginpage.php
+++ loginpage.php
@@ -3,13 +3,16 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>HTML Form</title>
+        <title>Login Page</title>
     </head>
     <body>
+        <img src="images/logogreen.jpg" alt="JusTTravel" height="100" width="100"><br>
             <form action="index.php" method="post">
+                Sign up to our website! <br><br>
                 Name: <input type="text" name="name"/><br>
                 Email: <input type="text" name="email"/><br>
                 Username: <input type ="text" name="username"/><br>
+                Password: <input type="password" name="password" size="10"/><br>
                     Location: <select name="locations">
                         <option value="United States" SELECTED> United States
                         <option value="Canada"> North America
@@ -25,7 +28,13 @@
                     <input type="RADIO" name="travelAmount" value="Average"> When I can
                     <input type="RADIO" name="travelAmount" value="Not Often"> I need to get out more often
                     <br><br>
-                    <input type="submit" name="submit_button" value="Sign Up"/>
+                    <input type="submit" name="signup_button" value="Sign Up"/>
+                    <br><br>
+                    
+                     Already have an account? <br>
+                     Username: <input type="text" name="username"/><br>
+                    Password: <input type="password" name="password" /><br>
+                    <input type="submit" name="login_button" value="Login">
             </form>  
     </body>
 </html>
