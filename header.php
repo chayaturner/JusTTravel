9295160c2045053580ad8162245b04b2bc9511ea
elise rozenberg <eliserozenberg@gmail.com>
2016-10-09 23:04:08 -0400

    added image ot header

--- header.php
+++ header.php
@@ -1,10 +1,15 @@
 <HTML>
-<HEAD>
-<LINK rel="stylesheet" href="styles.css" type="text/css" />
-</HEAD>
-<BODY>
-    <P><IMG src="images/logogreen.jpg" alt="JusTTravel" width="150" height="150"></P>
-    <P id="slogan"> "Plan less. Travel more." </P>
-<?php include "menu.php"; ?>
-<HR />
+    <HEAD>
+        <LINK rel="stylesheet" href="styles.css" type="text/css" />
+    </HEAD>
+    <BODY>
+
+        <DIV class="image">
+            <IMG src="images/beach1.jpg" alt="Beach" width="100%" height="350">
+            <IMG id ="logo" src="images/logogreen.jpg" alt="JusTTravel" width="150" height="150">
+
+            <P id="slogan"> "Plan less. Travel more." </P>
+               <?php include "menu.php"; ?>
+        </div>
+        <HR />
 
