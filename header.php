b9da69843c4d62e158c33e36a47ee58fda38665a
elise rozenberg <eliserozenberg@gmail.com>
2016-10-24 23:12:26 -0400

    added account name to  header

--- header.php
+++ header.php
@@ -1,3 +1,5 @@
+
+
 <HTML>
     <HEAD>
         <LINK rel="stylesheet" href="styles.css" type="text/css" />
@@ -7,9 +9,16 @@
         <DIV class="image">
             <IMG src="images/beach1.jpg" alt="Beach" width="100%" height="350">
             <IMG id ="logo" src="images/logogreen.jpg" alt="JusTTravel" width="150" height="150">
-
+            <?php
+            if ($_SESSION['LoggedIn']== true) {
+                echo "<p id='hello'>Hello ".$_COOKIE['username']."</p>";
+            }
+            else{
+                echo "<meta http-equiv='refresh' content='5;loginpage.php' />";
+            }
+            ?>
             <P id="slogan"> "Plan less. Travel more." </P>
-               <?php include "menu.php"; ?>
+                <?php include "menu.php"; ?>
         </div>
         <HR />
 
