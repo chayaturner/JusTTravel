d9bb0e466be34b1a6deb76a77b395a947d65a41b
elise rozenberg <eliserozenberg@gmail.com>
2016-10-31 04:07:57 -0400

    added database login

--- loginpage.php
+++ loginpage.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 
+
 <html>
     <head>
         <meta charset="UTF-8">
@@ -9,31 +10,6 @@
     <body>
         <img src="images/logogreen.jpg" alt="JusTTravel" height="100" width="100"><br>
             <form action="responsepage.php" method="post">
-                <!--<p class="loginheaders">Sign up to our website! </p>
-                Name: <input type="text" name="name"/><br>
-                Email: <input type="text" name="email"/><br>
-                Username: <input type ="text" name="username"/><br>
-                Password: <input type="password" name="password" size="10"/><br>
-                    Location: <select name="locations">
-                        <option value="United States" SELECTED> United States
-                        <option value="Canada"> North America
-                        <option value="Central America"> Central America
-                        <option value="South America"> South America
-                        <option value="Europe"> Europe
-                        <option value="Asia"> Asia
-                        <option value="Africa"> Africa
-                        <option value="Australia"> Australia
-                        <option value="Other"> Other
-                    </select><br>
-                    How often do you travel? <br><input type="RADIO" name="travelAmount" value="Often"> Very often
-                    <input type="RADIO" name="travelAmount" value="Average"> An average amount
-                    <input type="RADIO" name="travelAmount" value="Not Often"> I need to get out more often
-                    <br><br>
-                    <input type="submit" name="signup_button" value="Sign Up"/>
-                    <br><br>
-                    
-                    <p class="loginheaders">Already have an account? </p>
-                -->
                 <p class="loginheaders">Login</p>
                      Username: <input type="text" name="username"/><br>
                      Password: <input type="password" name="password" /><br><br>
@@ -41,3 +17,5 @@
             </form>  
     </body>
 </html>
+
+
--- responsepage.php
+++ responsepage.php
@@ -1,32 +1,60 @@
 <!DOCTYPE html>
 <html>  
     <body>
-        
-  <?php       
-    include 'loginpage.php';
 
-        $users = array();
-        $users[0] = array('username' =>'Chaya', 'password'=>'Turner');
-        $users[1] = array('username' =>'Elise', 'password'=>'Rozenberg');
-  
-        $loginArray = array('username'=> $_POST['username'], 'password'=> $_POST['password']);
-        
-        //check if user entered valid login info
-    if(in_array($loginArray, $users)){
-        //cookie that stores the username who logged in
-        setcookie('username', $loginArray['username']);
-        //set the session variable to true
-        $_SESSION['LoggedIn']= TRUE;
-        echo "<a href='index.php'> <br> Go! </a>";
-    } else {
-        //set the session varibale to false
-        $_SESSION['LoggedIn']= FALSE;
-      echo '<br>Incorrect username or password. 
+        <?php
+        require_once "dbConnect.php";
+        include 'loginpage.php';
+        session_start();
+
+        /*
+          $users = array();
+          $users[0] = array('username' => 'Chaya', 'password' => 'Turner');
+          $users[1] = array('username' => 'elise', 'password' => 'elise');
+
+          $loginArray = array('username' => $_POST['username'], 'password' => $_POST['password']);
+
+
+          //check if user entered valid login info
+          if (in_array($loginArray, $users)) {
+          //cookie that stores the username who logged in
+          setcookie('username', $loginArray['username']);
+          //set the session variable to true
+          $_SESSION['LoggedIn'] = TRUE;
+          echo "<a href='index.php'> <br> Go! </a>";
+          } else {
+          //set the session varibale to false
+          $_SESSION['LoggedIn'] = FALSE;
+          echo '<br>Incorrect username or password.
+          Please <a href="loginpage.php"> try again. </a>';
+          }
+         */
+
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $dbquery = sprintf("select username, password from user where email='%s' and password='%s'", $username, $password);
+        $dbresult = mysql_query($dbquery, $dbhandle);
+
+        if ($dbresult) {
+            //cookie that stores the username who logged in
+            setcookie('username', $username);
+            //set the session variable to true
+            $_SESSION['LoggedIn'] = TRUE;
+            echo "<a href = 'index.php'> <br> Go!</a>";
+        } else {
+            //set the session varibale to false
+            $_SESSION['LoggedIn'] = FALSE;
+            echo '<br>Incorrect username or password. 
            Please <a href="loginpage.php"> try again. </a>';
-    }
-    ?>    
+        }
+        ?>    
     </body>
 
 </html>
-    
-        
+
+
+
+
+
