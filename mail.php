1cae504306c45a5c59b5b9a21ae657ddfda3beaf
elise rozenberg <eliserozenberg@gmail.com>
2016-11-13 20:15:21 -0500

    added mailmaster files and fin mail.php

--- mail.php
+++ mail.php
@@ -1,6 +1,5 @@
 <?php
 
-echo $_POST['name'] . $_POST['email'] , $_POST['subject'] . $_POST['comment'];
 require 'PHPMailer-master/PHPMailerAutoload.php';
 $mail = new PHPMailer;
 $mail->isSMTP();
@@ -10,10 +9,14 @@ $mail->Host = 'smtp.gmail.com';
 $mail->Port = 465;
 $mail->Username = 'help.justtravel@gmail.com';
 $mail->Password = 'justtravel123';
-$mail->setFrom($_POST['email']);
-$mail->addAddress('help.justtravel@gmail.com',$_POST['name']);
-$mail->Subject = $_POST['subject'];
-$mail->Body = $_POST['comment'];
+$mail->setFrom('help.justtravel@gmail.com');
+$mail->addAddress('help.justtravel@gmail.com');
+$mail->addAddress($_POST['email'], $_POST['name']);
+$mail->Subject = $_POST['name'] . ': ' . $_POST['subject'];
+$mail->Body =("Thank you for your feedback. Please give us time to review your email."
+        ."\n\n\n______________________________________________________"
+        . "\n\nSent by:" . $_POST['name']. "\nCreate Date:". date('Y-m-d H:i:s').
+        "\n\n" . $_POST['comment']);
 //send the message, check for errors
 if (!$mail->send()) {
     echo "ERROR: " . $mail->ErrorInfo;
