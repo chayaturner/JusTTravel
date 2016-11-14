178abc21b75035f7103625b65ffb06d5e00ac3ba
elise rozenberg <eliserozenberg@gmail.com>
2016-11-13 20:46:51 -0500

    validated emails

--- contactus.php
+++ contactus.php
@@ -2,11 +2,12 @@
 
 <P class="content"> Have any questions or comments?????</p>
 <p>SEND US AN EMAIL!!!</P>
+
 <form action="mail.php" method="post" id="emailform">
-    Name: <input type="text" name="name"/><br>
-    Email: <input type="text" name="email"/><br>
-    Subject: <input type ="text" name="subject"/><br>
-    <textarea name="comment" form="emailform"></textarea>
+    Name: <input type="text" name="name" required="Oops! you forgot to tell us your name"/><br>
+    Email: <input type="email" name="email" required placeholder="Enter a valid email address"/><br>
+    Subject: <input type ="text" name="subject" required="Oops! you forgot to add the email subject" /><br>
+    <textarea name="comment" form="emailform" required ></textarea>
     <br>
     <input type="submit" name="send_button" value="Send Message"/>
 </form>  
--- mail.php
+++ mail.php
@@ -1,5 +1,7 @@
 <?php
 
+include "header.php";
+
 require 'PHPMailer-master/PHPMailerAutoload.php';
 $mail = new PHPMailer;
 $mail->isSMTP();
@@ -11,7 +13,7 @@ $mail->Username = 'help.justtravel@gmail.com';
 $mail->Password = 'justtravel123';
 $mail->setFrom('help.justtravel@gmail.com');
 $mail->addAddress('help.justtravel@gmail.com');
-$mail->addAddress($_POST['email'], $_POST['name']);
+$mail->addCC($_POST['email'], $_POST['name']);
 $mail->Subject = $_POST['name'] . ': ' . $_POST['subject'];
 $mail->Body =("Thank you for your feedback. Please give us time to review your email."
         ."\n\n\n______________________________________________________"
@@ -19,7 +21,8 @@ $mail->Body =("Thank you for your feedback. Please give us time to review your e
         "\n\n" . $_POST['comment']);
 //send the message, check for errors
 if (!$mail->send()) {
-    echo "ERROR: " . $mail->ErrorInfo;
+    echo "Email could not be sent - please check your email address and try again";
 } else {
-    echo "SUCCESS";
+    echo "Email was Sent!! Please check your inbox for confirmation";
 }
+ include "footer.php";
