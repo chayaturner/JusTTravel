95a93193e8106ad32f5c25f6738da1fc0c612e74
elise rozenberg <eliserozenberg@gmail.com>
2016-11-17 01:56:22 -0500

    updated dropdown in add act

--- addActivity.php
+++ addActivity.php
@@ -1,42 +1,44 @@
 <!DOCTYPE hmtl>
-<?php include "header.php"; ?>
-<html>
-    <header>
-        <meta charset="UTF-8">
-        <title>Create Activity Page</title>
-        <LINK rel="stylesheet" href="styles.css" type="text/css" />
-    </header>
-    <body>
-        <form action="addConfirmation.php" method="post">
-            <!-- Allow users to enter info about new Activity -->
-            <!-- all data should be entered into database -->
-            
-            CREATE NEW ACTIVITY!<br><br>
-           
-            Location: <select name="location">
-                        <option value="None" SELECTED> None
-                        <!-- input values from database -->
-            </select> 
-            Or new Location: <input type="TEXT" name ="newLocation" id="newLocation" size="30"<br><br>
-            <br>
-            Activity: <select name="activity">
-                        <option value="None" SELECTED> None
-                        <!--input values from database-->
-            </select> 
-            Or new Activity: <input type="TEXT" name ="newActivity" id="newActivity" size="30"<br><br>
-            <br><br>
-            
-            Company info: <br>
-            Name: <input type ="TEXT" name="coName" id="coName" size="30"><br>
-            Address: <input type ="TEXT" name="coAddress" id="coAddress" size="30"><br>
-            Phone Number: <input type ="TEXT" name="coPhone" id="coPhone" size="30"><br>
-            Email: <input type ="TEXT" name="coEmail" id="coEmail" size="30"><br><br>
-            
-            <input type="submit" name="create_button" value="Add Activity"/>
-        </form>
-    </body>
+<?php
+include "header.php";
+require_once "dbConnect.php";
+?>
+
+<form action="addConfirmation.php" method="post">
+    <!-- Allow users to enter info about new Activity -->
+    <!-- all data should be entered into database -->
+
+    CREATE NEW ACTIVITY!<br><br>
+
+    <?php
+    $dbquery = 'select statename from location';
+    $dbresult = mysql_query($dbquery, $dbhandle);
+    echo 'Location: <select name = "locations">';
+    echo '<option value="None" SELECTED> None</option>';
+    while ($dbrow = mysql_fetch_assoc($dbresult)) {
+        echo sprintf("<option value=%s>%s</option>", $dbrow['statecode'], $dbrow['statename']);
+    }
+    echo '</select><br>';
+
+    $dbquery = 'select activitytypeid,activitytypename from activitytype';
+    $dbresult = mysql_query($dbquery, $dbhandle);
+    echo 'Activity Type: <select name = "activityType">';
+    echo '<option value="Add" SELECTED>Add New</option>';
+    while ($dbrow = mysql_fetch_assoc($dbresult)) {
+        echo sprintf("<option value=%f>%s</option>", $dbrow['activitytypeid'], $dbrow['activitytypename']);
+    }
+    echo '</select>   Or new Activity: <input type = "TEXT" name = "newActivity" id = "newActivity" size = "30"<br><br>';
+    ?>
+    Company info: <br>
+    Name: <input type ="TEXT" name="coName" id="coName" size="30"><br>
+    Address: <input type ="TEXT" name="coAddress" id="coAddress" size="30"><br>
+    Phone Number: <input type ="TEXT" name="coPhone" id="coPhone" size="30"><br>
+    Email: <input type ="TEXT" name="coEmail" id="coEmail" size="30"><br><br>
+
+    <input type="submit" name="create_button" value="Add Activity"/>
+</form>
+</body>
 </html>
 
 <?php include "footer.php"; ?>
-
 
--- loginResponse.php
+++ loginResponse.php
@@ -1,56 +1,54 @@
 <!DOCTYPE html>
 
-<?php include "header.php"; ?>
+<?php
+include "header.php";
+require_once "dbConnect.php";
+session_start();
+
+/*
+  $users = array();
+  $users[0] = array('username' => 'Chaya', 'password' => 'Turner');
+  $users[1] = array('username' => 'elise', 'password' => 'elise');
+
+  $loginArray = array('username' => $_POST['username'], 'password' => $_POST['password']);
 
 
-        <?php
-        require_once "dbConnect.php";
-        session_start();
+  //check if user entered valid login info
+  if (in_array($loginArray, $users)) {
+  //cookie that stores the username who logged in
+  setcookie('username', $loginArray['username']);
+  //set the session variable to true
+  $_SESSION['LoggedIn'] = TRUE;
+  echo "<a href='index.php'> <br> Go! </a>";
+  } else {
+  //set the session varibale to false
+  $_SESSION['LoggedIn'] = FALSE;
+  echo '<br>Incorrect username or password.
+  Please <a href="loginpage.php"> try again. </a>';
+  }
+ */
 
-        /*
-          $users = array();
-          $users[0] = array('username' => 'Chaya', 'password' => 'Turner');
-          $users[1] = array('username' => 'elise', 'password' => 'elise');
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-          $loginArray = array('username' => $_POST['username'], 'password' => $_POST['password']);
+$dbquery = sprintf("select username from user where username='%s' and password='%s'", $username, $password);
+$dbresult = mysql_query($dbquery, $dbhandle);
+$num_rows = mysql_num_rows($dbresult);
 
-
-          //check if user entered valid login info
-          if (in_array($loginArray, $users)) {
-          //cookie that stores the username who logged in
-          setcookie('username', $loginArray['username']);
-          //set the session variable to true
-          $_SESSION['LoggedIn'] = TRUE;
-          echo "<a href='index.php'> <br> Go! </a>";
-          } else {
-          //set the session varibale to false
-          $_SESSION['LoggedIn'] = FALSE;
-          echo '<br>Incorrect username or password.
-          Please <a href="loginpage.php"> try again. </a>';
-          }
-         */
-
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-
-        $dbquery = sprintf("select username from user where username='%s' and password='%s'", $username, $password);
-        $dbresult = mysql_query($dbquery, $dbhandle);
-        $num_rows = mysql_num_rows($dbresult);
-
-        if ($num_rows == 1) {
-            //cookie that stores the username who logged in
-            setcookie('username', $username);
-            //set the session variable to true
-            $_SESSION['LoggedIn'] = TRUE;
-            header('Location: myActivities.php');
-        } else {
-            //set the session varibale to false
-            $_SESSION['LoggedIn'] = FALSE;
-            $_SESSION["Login.Error"] = 'Invalid credentials';
-            header('Location: login.php');
-        }
-        ?>    
-    </body>
+if ($num_rows == 1) {
+    //cookie that stores the username who logged in
+    setcookie('username', $username);
+    //set the session variable to true
+    $_SESSION['LoggedIn'] = TRUE;
+    header('Location: myActivities.php');
+} else {
+    //set the session varibale to false
+    $_SESSION['LoggedIn'] = FALSE;
+    $_SESSION["Login.Error"] = 'Invalid credentials';
+    header('Location: login.php');
+}
+?>    
+</body>
 
 </html>
 
