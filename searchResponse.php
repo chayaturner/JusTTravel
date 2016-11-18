1410eb44e52bf5d199223b28bba1f3fa3476f37e
elise rozenberg <eliserozenberg@gmail.com>
2016-11-18 01:41:28 -0500

    added rating to q

--- searchResponse.php
+++ searchResponse.php
@@ -6,27 +6,29 @@ require_once "dbConnect.php";
 if ($_POST['search'] == 'Search') {
     if ($_POST['location'] == 'None') {
         if ($_POST['activityType'] == 'None') {
-            $dbquery = 'select activityname, description from activity inner join activitytype on activitytype.ActivityTypeID = activity.ActivityTypeID inner join location on location.LocationID = activity.LocationID';
+            $dbquery = 'select rating, activityname, description,statename from activity inner join activitytype on activitytype.ActivityTypeID = activity.ActivityTypeID inner join location on location.LocationID = activity.LocationID';
         } else {
-            $dbquery = 'select activityname, description from activity inner join activitytype on activitytype.ActivityTypeID = activity.ActivityTypeID inner join location on location.LocationID = activity.LocationID where activitytype.ActivityTypeID = ' . $_POST['activityType'];
+            $dbquery = 'select rating, activityname, description,statename from activity inner join activitytype on activitytype.ActivityTypeID = activity.ActivityTypeID inner join location on location.LocationID = activity.LocationID where activitytype.ActivityTypeID = ' . $_POST['activityType'];
         }
     } else {
         if ($_POST['activityType'] == 'None') {
-            $dbquery = 'select activityname, description from activity inner join activitytype on activitytype.ActivityTypeID = activity.ActivityTypeID inner join location on location.LocationID = activity.LocationID where location.locationid = ' . $_POST['location'];
+            $dbquery = 'select rating, activityname, description,statename from activity inner join activitytype on activitytype.ActivityTypeID = activity.ActivityTypeID inner join location on location.LocationID = activity.LocationID where location.locationid = ' . $_POST['location'];
         } else {
-            $dbquery = 'select activityname, description from activity inner join activitytype on activitytype.ActivityTypeID = activity.ActivityTypeID inner join location on location.LocationID = activity.LocationID where location.locationid = ' . $_POST['location'] . ' and activitytype.ActivityTypeID = ' . $_POST['activityType'];
+            $dbquery = 'select rating, activityname, description,statename from activity inner join activitytype on activitytype.ActivityTypeID = activity.ActivityTypeID inner join location on location.LocationID = activity.LocationID where location.locationid = ' . $_POST['location'] . ' and activitytype.ActivityTypeID = ' . $_POST['activityType'];
         }
     }
 } else {
-    $dbquery = 'select activityname, description from activity inner join activitytype on activitytype.ActivityTypeID = activity.ActivityTypeID inner join location on location.LocationID = activity.LocationID';
+    $dbquery = 'select activityname,rating, description,statename from activity inner join activitytype on activitytype.ActivityTypeID = activity.ActivityTypeID inner join location on location.LocationID = activity.LocationID';
 }
 
 $dbresult = mysql_query($dbquery, $dbhandle);
 
 while ($dbrow = mysql_fetch_assoc($dbresult)) {
     echo '<div class="list">';
-    echo '<a href=#>' . $dbrow['activityname'] .'</a><br>' . $dbrow['description'] . '<br>';
+    echo '<a href=#>' . $dbrow['activityname'] . '</a><br>' . $dbrow['description'] . '<br>' . $dbrow['statename'] . '<br>';
     echo '</div>';
+    //need to carry data about activity into actiicty page to display full activity 
+    //add more detail to display list?
 }
 
 include "footer.php";
